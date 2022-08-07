c789af813172a10f4e93813835ad218bce8fac4a
Dave Skender <8432125+DaveSkender@users.noreply.github.com>
2022-08-07 02:00:45 -0400

    add base tables to database

---
+++ database/dbo/Tables/Definition.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Definition] (
+    [ID]           INT            IDENTITY (1, 1) NOT NULL,
+    [Word]         VARCHAR (100)  NOT NULL,
+    [PartOfSpeech] VARCHAR (10)   NULL,
+    [Definition]   VARCHAR (5000) NULL,
+    CONSTRAINT [PK_Definition] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Definition_Word] FOREIGN KEY ([Word]) REFERENCES [dbo].[Word] ([Word])
+);
+
